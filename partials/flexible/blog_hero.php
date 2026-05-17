d81ca683f2e6c8a14100ae0cb44fb59fb5b3c35c
Darinel Nieto <darinelnieto@Darinels-MacBook-Pro.local>
2026-05-17 09:05:12 -0500

    Authors

--- partials/flexible/blog_hero.php
+++ partials/flexible/blog_hero.php
@@ -11,6 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $hero = get_sub_field('hero_blog_partial');
 $tag = $hero['label_tag'];
 $img = $hero['main_image'];
+$author = $hero['author'];
 ?>
 <section class="blog-hero-partial-f87f86">
     <div class="container">
@@ -28,6 +29,18 @@ $img = $hero['main_image'];
                     <p class="description">
                         <?= $hero['description'] ?? ''; ?>
                     </p>
+                <?php endif; if($hero['enable_autor'] == 'true'): $link = $author['link']; ?>
+                    <a href="<?= $link['url'] ?? '#'; ?>" target="<?= $link['target'] ?? '_self'; ?>" class="author-tag">
+                        <?= wp_get_attachment_image($author['photo'], 'medium', false, array(
+                            'fetchpriority' => 'high',
+                            'class' => 'author-photo'
+                        )) ?>
+                        <div class="<?php if($author['label'] == 'rol'): ?>texts-column<?php else: ?>texts-flex<?php endif; ?>">
+                            <span class="name"><?= $author['name'] ?? '' ?></span>
+                            <?php if($author['label'] == 'date'): ?><span class="separator"></span><?php endif; ?>
+                            <span class="label"><?= $author['rol_or_date'] ?? '' ?></span>
+                        </div>
+                    </a>
                 <?php endif; ?>
             </div>
         </div>
